ade518b9b8d5042e9752f84b60f8d9f6cb1f3cdf
Anthony Gégo <anthonygego@hotmail.com>
2025-09-17 17:19:33 +0200

    [scripts/webdav] ensure WsgiDAV receive bounded input from werkzeug HTTP server

--- inginious/scripts/webdav.py
+++ inginious/scripts/webdav.py
@@ -12,6 +12,7 @@ import logging
 import os
 import sys
 from werkzeug.serving import run_simple
+from werkzeug.wsgi import get_input_stream
 
 # If INGInious files are not installed in Python path
 sys.path.append(os.path.dirname(__file__))
@@ -20,6 +21,13 @@ from inginious.common.log import init_logging, CustomLogMiddleware
 from inginious.common.base import load_json_or_yaml
 import inginious.frontend.webdav
 
+
+def limited_input_middleware(app):
+    # Ensure wsgi.input is a bounded stream
+    def new_app(environ, start_response):
+        environ['wsgi.input'] = get_input_stream(environ)
+        return app(environ, start_response)
+    return new_app
 
 def main():
     # Parse the paramaters from command line arguments
@@ -58,6 +66,9 @@ def main():
 
     # Add static redirection and request log
     application = CustomLogMiddleware(application, logging.getLogger("inginious.webdav.requests"))
+
+    # Ensure WsgiDAV receive limited streams for PUT requests
+    application = limited_input_middleware(application)
 
     # Launch the app
     run_simple(host, port, application, use_debugger=config.get("web_debug", False), threaded=True)
